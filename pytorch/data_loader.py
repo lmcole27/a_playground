233b85c04a81200c94315f898fd71d5cbd0b2cbb
Linda Cole <lindacolenl@gmail.com>
2026-04-02 16:41:41 -0400

    Added prediction

--- pytorch/data_loader.py
+++ pytorch/data_loader.py
@@ -1,12 +1,16 @@
 import torch
-from torch import nn
 from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent
+DATA_DIR = BASE_DIR / "data"
+
 
 # Download training data from open datasets.
 training_data = datasets.FashionMNIST(
-    root="data",
+    root=DATA_DIR,
     train=True,
     download=True,
     transform=ToTensor(),
@@ -14,7 +18,7 @@ training_data = datasets.FashionMNIST(
 
 # Download test data from open datasets.
 test_data = datasets.FashionMNIST(
-    root="data",
+    root=DATA_DIR,
     train=False,
     download=True,
     transform=ToTensor(),
@@ -25,3 +29,16 @@ batch_size = 64
 # Create data loaders.
 train_dataloader = DataLoader(training_data, batch_size=batch_size)
 test_dataloader = DataLoader(test_data, batch_size=batch_size)
+
+categories = [
+    "T-shirt/top",
+    "Trouser",
+    "Pullover",
+    "Dress",
+    "Coat",
+    "Sandal",
+    "Shirt",
+    "Sneaker",
+    "Bag",
+    "Ankle boot",
+]
--- pytorch/main.py
+++ pytorch/main.py
@@ -3,25 +3,24 @@ from torch import nn
 # from torch.utils.data import DataLoader
 # from torchvision import datasets
 # from torchvision.transforms import ToTensor
-# import matplotlib.pyplot as plt
 from datetime import datetime
 
-from data_loader import train_dataloader, test_dataloader
+from data_loader import train_dataloader, test_dataloader, training_data, test_data, categories
 from model import NeuralNetwork
 from train import train
 from test import test
 import os
+from pathlib import Path
+
 
 # x = torch.rand(5, 3)
 # print(x)
 
+BASE_DIR = Path(__file__).resolve().parent
+CHECKPOINTS_DIR = BASE_DIR / "checkpoints"
 
-# Display image and label.
+# Inspect a batch of data. x is a batch of images, y is a batch of labels
 for X, y in test_dataloader:
-    # img = X[0].squeeze()  # first image in batch
-    # plt.imshow(img, cmap="gray")
-    # plt.title(f"Label: {y[0].item()}")
-    # plt.show()
     print(f"Shape of X [N, C, H, W]: {X.shape}")
     print(f"Shape of y: {y.shape} {y.dtype}")
     break
@@ -32,27 +31,37 @@ print(f"Using {device} device")
 model = NeuralNetwork().to(device)
 print(model)
 
-if os.path.exists("checkpoints/latest_model.pth"):
-    model.load_state_dict(torch.load("checkpoints/latest_model.pth", map_location=device))
+if os.path.exists(CHECKPOINTS_DIR / f"latest_model.pth"):
+    model.load_state_dict(torch.load(CHECKPOINTS_DIR / f"latest_model.pth", map_location=device, weights_only=True))
     print("Loaded latest checkpoint")
+    Train = True
+    # Do not retrain if a checkpoint is found, just test the model on the test dataset. Unindent the following lines to retrain a model from the last checkpoint in addition
 else:
+    Train = True
     print("No checkpoint found, starting fresh")
+    # Train a model from scratch if no checkpoint is found
+ 
+if Train:
+    loss_fn = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
 
+    epochs = 5
+    for t in range(epochs):
+        print(f"Epoch {t+1}\n-------------------------------")
+        train(train_dataloader, model, loss_fn, optimizer, device)
+        test(test_dataloader, model, loss_fn, device)
+    print("Done!")
 
-loss_fn = nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    model_path = CHECKPOINTS_DIR / f"model_{timestamp}.pth"
+    torch.save(model.state_dict(), model_path)
+    torch.save(model.state_dict(), CHECKPOINTS_DIR / "latest_model.pth")
+    print(f"Saved model to {model_path}")
 
-
-epochs = 5
-for t in range(epochs):
-    print(f"Epoch {t+1}\n-------------------------------")
-    train(train_dataloader, model, loss_fn, optimizer, device)
-    test(test_dataloader, model, loss_fn, device)
-print("Done!")
-
-
-timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-model_path = f"checkpoints/model_{timestamp}.pth"
-torch.save(model.state_dict(), model_path)
-torch.save(model.state_dict(), "checkpoints/latest_model.pth")
-print(f"Saved model to {model_path}")
+model.eval()
+x, y = test_data[0][0], test_data[0][1]
+with torch.no_grad():
+    x = x.to(device)
+    pred = model(x)
+    predicted, actual = categories[pred[0].argmax(0)], categories[y]
+    print(f'Predicted: "{predicted}", Actual: "{actual}"')
